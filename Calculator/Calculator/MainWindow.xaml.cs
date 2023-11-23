7d70b6dd8a0760caacf57e939381f492968a8263
guillaume shahid <guillaume.shahid@gmail.com>
2023-11-23 07:48:40 +0100

    Make a WPF application using ListBox

---
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+
+		public Sum Sumobj { get; set;}
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			Sumobj = new Sum{ FirstNumber = "1", SecondNumber = "1"};
+			DataContext = Sumobj;
+		}
+	}
+}
---
+++ Calculator/Calculator/Sum.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Calculator
+{
+	public class Sum : INotifyPropertyChanged
+	{
+		private string _firstNumber;
+		private string _secondNumber;
+		private string _result;
+
+		public string FirstNumber
+		{
+			get { return _firstNumber; }
+			set
+			{
+				int number;
+				if(int.TryParse(value, out number))
+				{
+					_firstNumber = value;
+					OnPropertyChanged("FirstNumber");
+					OnPropertyChanged("Result");
+				}
+			}
+		}
+
+		public string SecondNumber
+		{
+			get { return _secondNumber; }
+			set
+			{
+				int number;
+				if(int.TryParse(value, out number))
+				{
+					_secondNumber = value;
+					OnPropertyChanged("SecondNumber");
+					OnPropertyChanged("Result");
+				}
+			}
+		}
+
+		public string Result
+		{
+			get 
+			{ 
+				int res = int.Parse(FirstNumber) + int.Parse(SecondNumber);
+				return res.ToString();					
+			}
+			set
+			{
+				int res = int.Parse(FirstNumber) + int.Parse(SecondNumber);
+				_result = res.ToString();
+				OnPropertyChanged("Result");
+			}
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			if(PropertyChanged != null)
+			{
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
